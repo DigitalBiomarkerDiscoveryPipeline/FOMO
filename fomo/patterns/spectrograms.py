050909f287e9301ae0eb4501550c8c20f873a8ac
Harrison Kane <htk4@duke.edu>
2024-04-08 18:14:03 -0400

    Edited spectrogram parameters

--- fomo/patterns/spectrograms.py
+++ fomo/patterns/spectrograms.py
@@ -3,20 +3,52 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.signal import spectrogram
 
-def generate_spectrograms(missing_data_matrix):
+
+# generate_spectrogram(person_data, missingness_interval, window_length_minutes, noverlap=None, zero_pad=0) instead, 
+# sampling frequency = 1/missingness_interval (from flag_missing_data())
+# calculate nperseg from window_length_minutes / missingness_interval
+# for anything above zero_pad=0 make nfft=nperseg+zero_pad
+
+def generate_spectrogram(person_missing_data, missingness_interval, window_length_minutes, noverlap=None, zero_pad=0):
+
+    """
+    Generate spectrogram from the missingness data of one person. Returns frequency, time, Sxx from scipy.siginal.spectrogram
+
+    person_missing_data - str, missingness data for one person
+    missingness_interval - Integer, how big of a gap there is between data points in output table in minutes (aggregated
+    raw data points into 'bins' of size interval_size)
+    window_length_minutes - Integer, how many minutes there are of data in person_missing_data
+    noverlap - for scipy.signal.spectrogram()
+    zero_pad - Integer, how many zeros to pad for FFT
+    """
+
+    # Calculate sampling frequency
+    fs = 1 / missingness_interval
+
+    # Calculate nperseg
+    nperseg = window_length_minutes / missingness_interval
+
+    # Calculate nfft
+    nfft = None if zero_pad == 0 else nperseg+zero_pad
+
+    # Get spectrogram
+    freq, time, Sxx = spectrogram(person_missing_data, fs, nperseg=nperseg, noverlap=noverlap, nfft=nfft)
+
+    return freq, time, Sxx
+
+
+def generate_spectrograms(missing_data_matrix, missingness_interval, window_length_minutes, noverlap=None, zero_pad=0):
     freqs_arr = []
     times_arr = []
     Sxx_arr = []
 
-    fs = 1 / (missing_data_matrix.columns[2] - missing_data_matrix.columns[1]).total_seconds()   # Sampling frequency
-
     for i in range(len(missing_data_matrix['person_id'])):
     
         # Generate spectrogram
-        signal = missing_data_matrix.loc[i][1:]
-        signal = signal.T.values
+        person_missing_data = missing_data_matrix.loc[i][1:]
+        person_missing_data = person_missing_data.T.values
 
-        frequencies, times, Sxx = spectrogram(signal, fs)
+        frequencies, times, Sxx = generate_spectrogram(person_missing_data, missingness_interval, window_length_minutes, noverlap, zero_pad)
 
         # Keep freq, time, Sxx for plotting spectrogram later
         freqs_arr.append(frequencies)
