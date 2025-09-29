dd487ad0464a1059b145700a96e7e713a803527d
hershkovichl <leeor.hershkovich@gmail.com>
2025-09-29 13:05:14 -0400

    Week level patterns and visualizations

--- fomo/viz/cyclical_pattern_plots.py
+++ fomo/viz/cyclical_pattern_plots.py
@@ -1,9 +1,8 @@
 from tslearn.barycenters import softdtw_barycenter
 
-def plot_time_series_by_cluster(time_series, labels, ylim=(0,1)):
+def plot_time_series_by_cluster(time_series, labels, ylim=(0,1), ncols=5):
     """Patterns as line plots"""
     unique_labels = np.unique(labels)
-    ncols = 5
     nrows = int(np.ceil(len(unique_labels) / ncols))
 
     fig, axs = plt.subplots(nrows, ncols, figsize=(12, 3 * nrows))
@@ -35,15 +34,25 @@ def plot_cluster_pattern_plots(time_series, labels, noscale_labels = [], ncols =
             scaled_barycenter = barycenter.flatten()
         else:
             scaled_barycenter = MinMaxScaler().fit_transform(barycenter).flatten()
-        cax = ax.pcolormesh(np.expand_dims(scaled_barycenter,axis=0), cmap='Blues_r',
+        if len(scaled_barycenter.shape) == 2: # day level plotting
+            cax = ax.pcolormesh(np.expand_dims(scaled_barycenter,axis=0), cmap='Blues_r',
+                            vmin = 0,
+                            vmax = 1)#, edgecolors='k', linewidth=0.1)
+            ax.set_yticks([0,1])
+            ax.set_yticklabels([' ', ' '])
+
+        elif len(scaled_barycenter.shape) == 3: # week level plotting
+            cax = ax.pcolormesh(scaled_barycenter[::-1], cmap='Blues_r',
                            vmin = 0,
                            vmax = 1)#, edgecolors='k', linewidth=0.1)
+            ax.set_yticks(range(7))
+            ax.set_yticklabels([' ' for _ in range(7)])
         ax.set_xticks(np.linspace(0,len(scaled_barycenter),5, dtype=int))
         ax.set_xticklabels([0,6,12,18,24])
-        ax.set_yticks([0,1])
+        
         ax.set_title(f"n={len(clust_data)}")
-        ax.set_yticklabels([' ', ' '])
-
+    return fig, axes
+        
 ## usage:
 pts_wclust=read_from_cloud('2year_minrange02_dtw_4h_wclust.csv')
 aggloDTW_labels = pts_wclust['cluster'].replace({3: 0, 1 : 0, 2:0, 7 :1, 6:1})
