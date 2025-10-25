fae743e21a1a616c7d2b6cfbb7c65a01c5da0054
hershkovichl <leeor.hershkovich@gmail.com>
2025-10-25 15:48:53 -0400

    Initial code, to be formatted

--- fomo/cluster/cyclical_clustering.py
+++ fomo/cluster/cyclical_clustering.py
@@ -25,3 +25,16 @@ axes[0].set_ylabel('Wear Level (Z score)')
 for ax in axes:
     ax.set_title('')
     ax.set_xlabel('Hour of day')
+
+### Week level cyclical clustering
+data_matrix = np.load('2year_dayweek_avg_full.npy')
+# dist_matrix_full = np.load('2year_variable030_dayweek_dtw_4h_radius.npy')
+
+min_range = 0.3
+row_ranges = np.ptp(full_data_matrix, axis=1)
+variable_rows = full_data_matrix[row_ranges >= min_range]
+scaled_variable_full = StandardScaler(with_std=True).fit_transform(variable_rows.T).T
+
+aggloDTW = AgglomerativeClustering(n_clusters=None, metric='precomputed', linkage='complete', compute_full_tree=True, distance_threshold=170)
+aggloDTW_labels = aggloDTW.fit_predict(dist_matrix_full)
+np.array(np.unique(aggloDTW_labels, return_counts=True)).T
