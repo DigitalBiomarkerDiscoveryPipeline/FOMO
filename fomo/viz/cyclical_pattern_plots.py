98e0171afc78f43ef1658174ddff016d47d02d8e
hershkovichl <leeor.hershkovich@gmail.com>
2025-10-25 20:54:47 -0400

    Proper data reshaping for week-level data

--- fomo/viz/cyclical_pattern_plots.py
+++ fomo/viz/cyclical_pattern_plots.py
@@ -25,15 +25,16 @@ def plot_time_series_by_cluster(time_series, labels, ylim=(0,1), ncols=5):
         axs.flatten()[i].set_title(f"Time Series in Cluster {label}")
     return fig, axs
 
-def cluster_pattern_plots(time_series_matrix, labels, axes = None, noscale_labels = [], ncols = 1, show_colorbar=True, gamma=1.):
+def cluster_pattern_plots(pattern_matrix, labels, axes = None, noscale_labels = [], ncols = 1, show_colorbar=True, gamma=1.):
     '''Plot day-level (1D) or week-level (2D) pattern plot heatmaps as the barycenter of each cluster.
     
     Parameters
     ----------
-    time_series_matrix: np.array
-        2-D matrix of time series data, which has shape ([number of time series in matrix], [length of each time series])
+    pattern_matrix: np.array
+        2-D or 3-D matrix of patterns, which has shape ([number of time series in matrix], [length of each time series]) for 2D
+        Or shape ([number of time series in matrix], [number of rows of time series], [length of each row of time series])
     labels: array-like
-        Array of labels corresponding to each time series in `time_series_matrix`
+        Array of labels corresponding to each time series in `pattern_matrix`
     axes: matplotlib Axes array (default: None)
         Array of matplotlib axes for plotting the heatmaps. If None is provided, a fig and axes will be created.
     noscale_labels: list (default: [])
@@ -53,7 +54,7 @@ def cluster_pattern_plots(time_series_matrix, labels, axes = None, noscale_label
     nrows = int(np.ceil(len(unique_labels) / ncols))
 
     if axes is None:
-        if len(time_series_matrix.shape) == 3:
+        if len(pattern_matrix.shape) == 3:
             figsize = (4*ncols, 2*nrows) # Make week level plots taller
         else:
             figsize = (4*ncols, 1*nrows) # Make day level plots shorter
@@ -64,13 +65,14 @@ def cluster_pattern_plots(time_series_matrix, labels, axes = None, noscale_label
             raise ValueError(f'Provided array of axes is too small. Expected at least {len(unique_labels)} axes, but provided axes array has only {np.prod(axes.shape)}')
     
     for label, ax in zip(unique_labels, axes.flatten()):
-        clust_data = time_series_matrix[labels == label]
+        clust_data = pattern_matrix[labels == label]
+        clust_data = clust_data.reshape(clust_data.shape[0], -1)
         barycenter = softdtw_barycenter(clust_data, gamma=gamma, max_iter=50, tol=1e-3)
         if label in noscale_labels:
             scaled_barycenter = barycenter.flatten()
         else:
             scaled_barycenter = MinMaxScaler().fit_transform(barycenter).flatten()
-        if len(time_series_matrix.shape) == 2: # day level plotting
+        if len(pattern_matrix.shape) == 2: # day level plotting
             cax = ax.pcolormesh(np.expand_dims(scaled_barycenter,axis=0), cmap='Blues_r',
                             vmin = 0,
                             vmax = 1)#, edgecolors='k', linewidth=0.1)
@@ -78,7 +80,7 @@ def cluster_pattern_plots(time_series_matrix, labels, axes = None, noscale_label
             ax.set_yticklabels([' ', ' '])
             ax.set_xticks(np.linspace(0,len(scaled_barycenter),5, dtype=int))
 
-        elif len(time_series_matrix.shape) == 3: # week level plotting
+        elif len(pattern_matrix.shape) == 3: # week level plotting
             scaled_barycenter = scaled_barycenter.reshape(7, -1)
             cax = ax.pcolormesh(scaled_barycenter[::-1], # invert y axis to make the ordering Sunday-Saturday from top to bottom
                                 cmap='Blues_r',
