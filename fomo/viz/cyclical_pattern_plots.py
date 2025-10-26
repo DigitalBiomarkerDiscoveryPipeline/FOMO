51d1f532fca1cb2951973627c3fd0d1c96686635
hershkovichl <leeor.hershkovich@gmail.com>
2025-10-25 20:12:55 -0400

    Properly formatted week-level pattern plots

--- fomo/viz/cyclical_pattern_plots.py
+++ fomo/viz/cyclical_pattern_plots.py
@@ -2,6 +2,8 @@ from tslearn.barycenters import softdtw_barycenter
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
+import matplotlib.cm as cm
+import matplotlib.colors as mcolors
 
 def plot_time_series_by_cluster(time_series, labels, ylim=(0,1), ncols=5):
     """Patterns as line plots"""
@@ -23,7 +25,7 @@ def plot_time_series_by_cluster(time_series, labels, ylim=(0,1), ncols=5):
         axs.flatten()[i].set_title(f"Time Series in Cluster {label}")
     return fig, axs
 
-def cluster_pattern_plots(time_series_matrix, labels, axes = None, noscale_labels = [], ncols = 1):
+def cluster_pattern_plots(time_series_matrix, labels, axes = None, noscale_labels = [], ncols = 1, show_colorbar=True):
     '''Plot day-level (1D) or week-level (2D) pattern plot heatmaps as the barycenter of each cluster.
     
     Parameters
@@ -38,6 +40,8 @@ def cluster_pattern_plots(time_series_matrix, labels, axes = None, noscale_label
         List of labels found in `labels` which should not be passed through MinMaxScaler
     ncols: int (default: 1)
         Number of columns in figure. The number of rows will be calculated to fit the number of unique patterns
+    show_colorbar: bool (default: True)
+        If true, shows a colorbar on the right of the figure. Colorbar will always use the full range of the cmap.
     
     Returns
     -------
@@ -49,7 +53,11 @@ def cluster_pattern_plots(time_series_matrix, labels, axes = None, noscale_label
     nrows = int(np.ceil(len(unique_labels) / ncols))
 
     if axes is None:
-        fig, axes = plt.subplots(nrows, ncols, figsize=(4*ncols, 1 * nrows))
+        if len(time_series_matrix.shape) == 3:
+            figsize = (4*ncols, 2*nrows) # Make week level plots taller
+        else:
+            figsize = (4*ncols, 1*nrows) # Make day level plots shorter
+        fig, axes = plt.subplots(nrows, ncols, figsize=figsize)
     else:
         fig = plt.gcf()
         if  np.prod(axes.shape) < len(unique_labels):
@@ -62,21 +70,30 @@ def cluster_pattern_plots(time_series_matrix, labels, axes = None, noscale_label
             scaled_barycenter = barycenter.flatten()
         else:
             scaled_barycenter = MinMaxScaler().fit_transform(barycenter).flatten()
-        if len(scaled_barycenter.shape) == 1: # day level plotting
+        if len(time_series_matrix.shape) == 2: # day level plotting
             cax = ax.pcolormesh(np.expand_dims(scaled_barycenter,axis=0), cmap='Blues_r',
                             vmin = 0,
                             vmax = 1)#, edgecolors='k', linewidth=0.1)
             ax.set_yticks([0,1])
             ax.set_yticklabels([' ', ' '])
 
-        elif len(scaled_barycenter.shape) == 2: # week level plotting
-            cax = ax.pcolormesh(scaled_barycenter[::-1], cmap='Blues_r',
+        elif len(time_series_matrix.shape) == 3: # week level plotting
+            scaled_barycenter = scaled_barycenter.reshape(7, -1)
+            cax = ax.pcolormesh(scaled_barycenter[::-1], # invert y axis to make the ordering Sunday-Saturday from top to bottom
+                                cmap='Blues_r',
                            vmin = 0,
                            vmax = 1)#, edgecolors='k', linewidth=0.1)
-            ax.set_yticks(range(7))
-            ax.set_yticklabels([' ' for _ in range(7)])
+            ax.set_yticks(np.arange(7) + 0.5)
+            ax.set_yticklabels(['Sa','F','R','W','T','M','Su'])
         ax.set_xticks(np.linspace(0,len(scaled_barycenter),5, dtype=int))
         ax.set_xticklabels([0,6,12,18,24])
         
+        
         ax.set_title(f"n={len(clust_data)}")
+        
+    if show_colorbar:
+        # Make a colorbar that always shows the full range of the colormap
+        norm = mcolors.Normalize(vmin=0, vmax=1)
+        mappable = cm.ScalarMappable(norm=norm, cmap='Blues_r')
+        fig.colorbar(mappable, ax=axes.flatten())
     return fig, axes
